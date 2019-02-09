034a0f3276ee4f09ac3cc0c969fa49a38853f896
Jomar <jomar.magno026@gmail.com>
2019-02-09 16:27:45 +0800

    setup graphql server

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+const events = [];
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+   schema: buildSchema(`
+      type Event {
+         _id: ID!
+         title: String!
+         description: String!
+         price: Float!
+         date: String!
+      }
+
+      input EventInput {
+         title: String!
+         description: String!
+         price: Float!
+         date: String!
+      }
+
+      type RootQuery {
+         events: [Event!]!
+      }
+
+      type RootMutation {
+         createEvent(eventInput: EventInput): Event
+      }
+
+      schema {
+         query: RootQuery
+         mutation: RootMutation
+      }
+   `),
+   rootValue: {
+      events: () => {
+         return events;
+      },
+      createEvent: (args) => {
+         const { title, description, price, date } = args.eventInput;
+         const event = {
+            _id: Math.random().toString(),
+            title: title,
+            description: description,
+            price: +price,
+            date: date
+         };
+
+         events.push(event);
+         return event;
+      }
+   },
+   graphiql: true
+}));
+
+app.listen(3000);
