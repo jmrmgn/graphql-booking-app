f7a9c7f9c6752f03bb5f7b8984ba105ae1dd01b3
Jomar <jomar.magno026@gmail.com>
2019-02-09 17:06:21 +0800

    event model added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-const app = express();
+const Event = require('./models/Event');
 
-const events = [];
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -40,24 +41,46 @@ app.use('/graphql', graphqlHttp({
       }
    `),
    rootValue: {
-      events: () => {
-         return events;
+      events: async () => {
+         try {
+            const events = await Event.find();
+            return events;
+         }
+         catch (error) {
+            console.log(error);
+         }
+
       },
       createEvent: (args) => {
-         const { title, description, price, date } = args.eventInput;
-         const event = {
-            _id: Math.random().toString(),
-            title: title,
-            description: description,
-            price: +price,
-            date: date
-         };
+         const { title, description, price } = args.eventInput;
+         const event = new Event({ title, description, price, date: new Date(args.eventInput.date) });
 
-         events.push(event);
-         return event;
+         return event
+            .save()
+            .then(result => {
+               console.log(result);
+               return {...result._doc};
+            })
+            .catch(err => {
+               console.log(err);
+               throw err;
+            });
+
       }
    },
    graphiql: true
 }));
 
-app.listen(3000);
+mongoose.connect(
+   'mongodb://localhost/graphql-booking-app-dev',
+   {
+      useNewUrlParser: true
+   }
+)
+.then(() => {
+   console.log('Connected to database');
+   app.listen(3000, () => {
+      console.log('Server running at port 3000')
+   });
+})
+.catch(err => console.log(err));
